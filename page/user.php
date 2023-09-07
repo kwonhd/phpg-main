a3ce1f42daeb98bebc05d84f8e086c7d086224d1
kwonhd <130338461+kwonhd@users.noreply.github.com>
2023-09-07 17:39:56 +0900

    phpg

---
+++ page/user.php
@@ -0,0 +1,33 @@
+<?php
+print_r($POST);
+print_r($_GET);
+    $title=array('User','사용자');
+    include('./module/header.php');
+    include('./module/mysql.php');
+    $qry='select id,name,uid,upw,uemail,status from users where id=1';
+    $user=getData($qry);
+    //print_r($user);
+    $udata=array(
+        array('id','번호','hidden'),
+        array('name','이름','txt'),
+        array('uid','아이디','txt'),
+        array('upw','패스워드','password'),
+        array('uemail','이메일','txt'),
+        array('status','상태','txt'),
+    );
+?>
+<form action="/page/users.php?do=edit" method="POST">
+    <?php foreach ($udata as $u) {
+        $txt = '<div class="form-group row">';
+        if ($u[2] != 'hidden') {
+            $txt .= '<label class="col-sm-2 col-form-label" for="' . $u[0] . '">' . $u[1] . ':</label>';
+        }
+        $txt .= '<div class="col-sm-10"> 
+                    <input type="' . $u[2] . '" class="form-control" 
+                    name="' . $u[0] . '" value="' . $user[$u[0]] . '">
+                </div>
+            </div>';
+        print($txt);
+    } ?>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
